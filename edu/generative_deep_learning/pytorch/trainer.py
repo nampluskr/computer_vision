4891927293e9b40c6144f1ec5c5492d8231d5ba8
nampluskr <nampluskr@gmail.com>
2025-11-05 00:16:58 +0900

    update

--- edu/generative_deep_learning/pytorch/trainer.py
+++ edu/generative_deep_learning/pytorch/trainer.py
@@ -1,5 +1,7 @@
+import numpy as np
 import torch
 from tqdm import tqdm
+
 
 def train(model, dataloader, optimizer):
     model.train()
@@ -10,8 +12,7 @@ def train(model, dataloader, optimizer):
         batch_size = batch["image"].shape[0]
         total_images += batch_size
 
-        outputs = model.training_step(batch)
-        loss = outputs["loss"]
+        outputs = model.train_step(batch, optimizer)
         for name, value in outputs.items():
             results.setdefault(name, 0.0)
             results[name] += value.item() * batch_size
@@ -41,7 +42,7 @@ def predict(model, dataloader):
     results_dict = {}
 
     for batch in dataloader:
-        outputs = model.pred_step(batch)  # {"image": ..., "label": ..., "pred": ..., }
+        outputs = model.pred_step(batch)  # {"images": ..., "labels": ..., "preds": ..., }
 
         for name, value in outputs.items():
             if torch.is_tensor(value):
@@ -65,3 +66,28 @@ def predict(model, dataloader):
             results[name] = np.array(value_list)
 
     return results
+
+
+def fit(model, train_loader, optimizer, num_epochs, valid_loader=None):
+    history = {"tarin": {}, "valid": {}}
+    for epoch in range(1, num_epochs + 1):
+        epoch_info = f"[{epoch:3d}/{num_epochs}]"
+        train_results = train(model, train_loader, optimizer)
+        train_info = ", ".join([f"{k}:{v:.3f}" for k, v in train_results.items()])
+
+        for key, value in train_results.items():
+            history["tarin"].setdefault(key, [])
+            history["tarin"][key].append(value)
+
+        if valid_loader is not None:
+            valid_results = evaluate(model, valid_loader)
+            valid_info = ", ".join([f"{k}:{v:.3f}" for k, v in valid_results.items()])
+
+            for key, value in train_results.items():
+                history["valid"].setdefault(key, [])
+                history["valid"][key].append(value)
+            print(f"{epoch_info} {train_info} | (val) {valid_info}")
+        else:
+            print(f"{epoch_info} {train_info}")
+
+    return history
