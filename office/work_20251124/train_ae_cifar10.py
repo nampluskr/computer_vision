56562f3987c145fe4e15824adc5fc4bc43640abe
nampluskr <37031766+nampluskr@users.noreply.github.com>
2025-11-25 15:47:44 +0900

    Create train_ae_cifar10.py

---
+++ office/work_20251124/train_ae_cifar10.py
@@ -0,0 +1,44 @@
+import torch
+from torchvision import transforms as T
+
+from datasets import CIFAR10, get_train_loader, get_test_loader
+from autoencoder_v2 import Encoder32, Decoder32, AutoEncoder
+from autoencoder_v2 import VEncoder32, VAE
+from trainer import fit
+from utils import set_seed
+
+
+SEED = 42
+BATCH_SIZE = 64
+LATENT_DIM = 128
+BASE = 64
+NUM_EPOCHS = 5
+
+
+if __name__ == "__main__":
+
+    if 0:
+        print(f"\n>> Autoencoder")
+
+        set_seed(SEED)
+        root_dir = "/home/namu/myspace/NAMU/datasets/cifar10"
+        train_loader = get_train_loader(dataset=CIFAR10(root_dir, "train"), batch_size=BATCH_SIZE)
+        test_loader = get_test_loader(dataset=CIFAR10(root_dir, "test"), batch_size=BATCH_SIZE)
+
+        encoder = Encoder32(in_channels=3, latent_dim=LATENT_DIM)
+        decoder = Decoder32(out_channels=3, latent_dim=LATENT_DIM)
+        ae = AutoEncoder(encoder, decoder)
+        history = fit(ae, train_loader, num_epochs=NUM_EPOCHS, valid_loader=test_loader)
+
+    if 1:
+        print(f"\n>> Variational Autoencoder")
+
+        set_seed(SEED)
+        root_dir = "/home/namu/myspace/NAMU/datasets/cifar10"
+        train_loader = get_train_loader(dataset=CIFAR10(root_dir, "train"), batch_size=BATCH_SIZE)
+        test_loader = get_test_loader(dataset=CIFAR10(root_dir, "test"), batch_size=BATCH_SIZE)
+
+        encoder = VEncoder32(in_channels=3, latent_dim=LATENT_DIM)
+        decoder = Decoder32(out_channels=3, latent_dim=LATENT_DIM)
+        vae = VAE(encoder, decoder)
+        history = fit(vae, train_loader, num_epochs=NUM_EPOCHS, valid_loader=test_loader)
