439f3e1e2d8b89e9f5a8617391caf6274382a2c8
nampluskr <37031766+nampluskr@users.noreply.github.com>
2025-12-05 15:17:28 +0900

    Create celeba_gan_bce.py

---
+++ office/work_20251205/01_gan/celeba_gan_bce.py
@@ -0,0 +1,66 @@
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+common_dir = os.path.join(os.path.dirname(current_dir), 'common')
+model_dir = os.path.join(os.path.dirname(current_dir), 'models')
+
+for path in [common_dir, model_dir]:
+    if path not in sys.path:
+        sys.path.append(path)
+
+#####################################################################
+import torch
+import torchvision.transforms as T
+
+
+from datasets import CelebA, get_train_loader
+from utils import set_seed, create_images, sample_latent, update_history, plot_images
+from generator import Generator64
+from discriminator import Discriminator64
+from gan import GAN
+from trainer import fit
+
+
+if __name__ == "__main__":
+
+    SEED = 42
+    DATA_DIR = "/home/namu/myspace/NAMU/datasets/celeba"
+    BATCH_SIZE = 64
+
+    set_seed(SEED)
+    transform = T.Compose([T.Resize((64, 64)), T.ToTensor(), T.Normalize(mean=[0.5]*3, std=[0.5]*3)])
+    dataset = CelebA(root_dir=DATA_DIR, split="train", transform=transform, attributes=["Male", "Smiling"])
+    train_loader = get_train_loader(dataset, batch_size=BATCH_SIZE)
+
+    LATENT_DIM = 100
+    IN_CHANNELS = 3
+    OUT_CHANNELS = 3
+    BASE = 64
+
+    generator = Generator64(latent_dim=LATENT_DIM, out_channels=OUT_CHANNELS, base=BASE)
+    discriminator = Discriminator64(in_channels=IN_CHANNELS, base=BASE)
+    gan = GAN(discriminator, generator, loss_type="bce")
+
+    NUM_EPOCHS = 2
+    TOTAL_EPOCHS = 10
+    NUM_SAMPLES = 100
+
+    FILENAME = os.path.splitext(os.path.basename(__file__))[0]
+    OUTPUT_DIR = f"./outputs_{FILENAME}"
+    IMAGE_NAME = FILENAME + ""
+
+    noises = sample_latent(NUM_SAMPLES, LATENT_DIM)
+    history = {}
+    epoch = 0
+    for _ in range(TOTAL_EPOCHS // NUM_EPOCHS):
+        epoch_history = fit(gan, train_loader, num_epochs=NUM_EPOCHS, total_epochs=TOTAL_EPOCHS)
+        update_history(history, epoch_history)
+
+        images = create_images(gan.generator, noises)
+        epoch += NUM_EPOCHS
+        image_path = os.path.join(OUTPUT_DIR, f"{IMAGE_NAME}_epoch{epoch:03d}.png")
+        plot_images(*images, ncols=10, xunit=1, yunit=1, save_path=image_path)
+
+
+
