c466262acf249ee59914ca716deda0b2e880d0f6
nampluskr <37031766+nampluskr@users.noreply.github.com>
2025-12-05 15:21:14 +0900

    Create mnist_cgan_bce.py

---
+++ office/work_20251205/04_cgan/mnist_cgan_bce.py
@@ -0,0 +1,75 @@
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+common_dir = os.path.join(os.path.dirname(current_dir), 'common')
+model_dir = os.path.join(os.path.dirname(current_dir), 'models')
+
+for path in [common_dir, model_dir]:
+    if path not in sys.path:
+        sys.path.append(path)
+
+#####################################################################
+import torch
+import torchvision.transforms as T
+
+
+from datasets import MNIST, get_train_loader
+from utils import set_seed, create_images, sample_latent, update_history, plot_images
+from generator import CGenerator32
+from discriminator import CDiscriminator32
+from generator import Generator32
+from discriminator import Discriminator32
+from cgan import CGAN, CGenerator, CDiscriminator
+from trainer import fit
+
+
+if __name__ == "__main__":
+
+    SEED = 42
+    DATA_DIR = "/home/namu/myspace/NAMU/datasets/mnist"
+    BATCH_SIZE = 128
+
+    set_seed(SEED)
+    transform = T.Compose([T.ToTensor(), T.Normalize(mean=[0.5], std=[0.5])])
+    dataset = MNIST(root_dir=DATA_DIR, split="train", transform=transform)
+    train_loader = get_train_loader(dataset, batch_size=BATCH_SIZE)
+
+    LATENT_DIM = 100
+    IN_CHANNELS = 1
+    OUT_CHANNELS = 1
+    BASE = 64
+    NUM_CLASSES = 10
+    EMBEDDING_DIM = 64
+    EMBEDDING_CHANNELS = 64
+
+    generator = CGenerator32(
+        generagor=Generator32(latent_dim=LATENT_DIM + EMBEDDING_DIM, out_channels=OUT_CHANNELS, base=BASE), 
+        num_classes=NUM_CLASSES, embedding_dim=EMBEDDING_DIM)
+    discriminator = CDiscriminator(
+        Discriminator32(in_channels=IN_CHANNELS + EMBEDDING_CHANNELS, base=BASE),
+        num_classes=NUM_CLASSES, embedding_channels=EMBEDDING_CHANNELS)
+    gan = CGAN(discriminator, generator, loss_type="bce")
+
+    NUM_EPOCHS = 2
+    TOTAL_EPOCHS = 10
+    NUM_SAMPLES = 100
+
+    FILENAME = os.path.splitext(os.path.basename(__file__))[0]
+    OUTPUT_DIR = f"./outputs_{FILENAME}"
+    IMAGE_NAME = FILENAME + ""
+
+    noises = np.random.normal((NUM_SAMPLES, LATENT_DIM))
+    history = {}
+    epoch = 0
+    for _ in range(TOTAL_EPOCHS // NUM_EPOCHS):
+        epoch_history = fit(gan, train_loader, num_epochs=NUM_EPOCHS, total_epochs=TOTAL_EPOCHS)
+        update_history(history, epoch_history)
+
+        images = create_images(gan.generator, noises)
+        epoch += NUM_EPOCHS
+        image_path = os.path.join(OUTPUT_DIR, f"{IMAGE_NAME}_epoch{epoch:03d}.png")
+        plot_images(*images, ncols=10, xunit=1, yunit=1, save_path=image_path)
+
+
+
